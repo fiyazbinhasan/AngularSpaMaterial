39eb397b9c422ad7636257aef733f74262d44487
fiyazbinhasan <fiyazhasan@live.com>
2017-10-10 20:33:47 +0600

    angular material added

---
+++ AngulaSpaMaterial/ClientApp/app/components/counter/counter.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'counter',
+    templateUrl: './counter.component.html'
+})
+export class CounterComponent {
+    public currentCount = 0;
+    public isEnabled = true;
+
+    public incrementCounter() {
+        this.currentCount++;
+    }
+
+
+}
